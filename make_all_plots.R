d39c1bcda004c944b383c503c96fc09700d550e8
scilix <scilix@dummy.com>
2020-03-29 15:13:41 +0200

    Wrapper script to create all plots at once

---
+++ make_all_plots.R
@@ -0,0 +1,19 @@
+# execute all scripts
+
+print("Executing plot 1")
+source("plot1.R")
+
+print("Executing plot 2")
+source("plot2.R")
+
+print("Executing plot 3")
+source("plot3.R")
+
+print("Executing plot 4")
+source("plot4.R")
+
+print("Executing plot 5")
+source("plot5.R")
+
+print("Executing plot 6")
+source("plot6.R")
